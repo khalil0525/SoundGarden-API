9bb135c57eb36c59f562f3807bf059fd10fac3d4
Khalil Collins <Collinskhalil@hotmail.com>
2023-12-31 17:15:27 -0500

    db changes for prod or local

--- db/db.js
+++ db/db.js
@@ -1,13 +1,30 @@
 const Sequelize = require('sequelize');
 require('dotenv').config();
 
-const db = new Sequelize({
-  database: process.env.DB_DATABASE,
-  username: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  dialect: 'postgres',
-});
+let db;
+
+if (process.env.DATABASE_URL) {
+  // Production environment with DATABASE_URL
+  db = new Sequelize(process.env.DATABASE_URL, {
+    dialect: 'postgres',
+    protocol: 'postgres',
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+    },
+  });
+} else {
+  // Development or other environments
+  db = new Sequelize({
+    database: process.env.DB_DATABASE,
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    dialect: 'postgres',
+  });
+}
 
 module.exports = db;
